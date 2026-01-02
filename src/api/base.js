1b09550fc06f0330457ca86dd5378e4d9659a56f
yasmin <yasmingargouri04@gmail.com>
2026-01-02 15:19:07 +0100

    checkpoint

--- src/api/base.js
+++ src/api/base.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_URL || "http://crm_backend.railway.internal/api";
-const PICTURE_URL = process.env.REACT_APP_PICTURE_URL || "http://crm_backend.railway.internal";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api";
+const PICTURE_URL = process.env.REACT_APP_PICTURE_URL || "http://localhost:4000";
 
 export { BASE_URL, PICTURE_URL };
